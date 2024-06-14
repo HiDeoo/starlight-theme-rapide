70e9988a572fa77c487e66c6b63a7c17323ded22
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-06-14 11:39:56 +0200

    feat: style language select

--- packages/starlight-theme-rapide/index.ts
+++ packages/starlight-theme-rapide/index.ts
@@ -18,6 +18,7 @@ export default function starlightThemeRapidePlugin(): StarlightPlugin {
                 fallback: 'Banner',
               },
               'ThemeSelect',
+              'LanguageSelect',
             ],
             logger,
           ),
