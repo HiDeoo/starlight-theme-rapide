aa21f6508789aebae3da6a43c36b41c73582b252
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-04-22 18:36:11 +0200

    docs: remove custom css

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   integrations: [
     starlight({
       credits: true,
-      customCss: ['./src/styles/custom.css'],
       editLink: {
         baseUrl: 'https://github.com/HiDeoo/starlight-theme-rapide/edit/main/docs/',
       },
