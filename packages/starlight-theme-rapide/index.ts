61b8e9c76f48b39516e3b5a41283bb2c0718362d
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-04-22 17:14:20 +0200

    fix: ec nested user settings

--- packages/starlight-theme-rapide/index.ts
+++ packages/starlight-theme-rapide/index.ts
@@ -7,6 +7,9 @@ export default function starlightThemeRapidePlugin(): StarlightPlugin {
     name: 'starlight-theme-rapide',
     hooks: {
       'config:setup'({ config, logger, updateConfig }) {
+        const userExpressiveCodeConfig =
+          !config.expressiveCode || config.expressiveCode === true ? {} : config.expressiveCode
+
         updateConfig({
           components: overrideComponents(config, ['LanguageSelect', 'Pagination', 'ThemeSelect'], logger),
           customCss: [
@@ -15,28 +18,30 @@ export default function starlightThemeRapidePlugin(): StarlightPlugin {
             'starlight-theme-rapide/styles/theme',
             'starlight-theme-rapide/styles/base',
           ],
-          // TODO(HiDeoo)
           expressiveCode:
             config.expressiveCode === false
               ? false
               : {
+                  themes: ['vitesse-dark', 'vitesse-light'],
+                  ...userExpressiveCodeConfig,
                   styleOverrides: {
                     borderColor: 'var(--sl-rapide-ui-border-color)',
                     borderRadius: '0.5rem',
+                    ...userExpressiveCodeConfig.styleOverrides,
                     frames: {
                       editorActiveTabIndicatorTopColor: 'unset',
                       editorActiveTabIndicatorBottomColor: 'var(--sl-color-gray-3)',
                       editorTabBarBorderBottomColor: 'var(--sl-rapide-ui-border-color)',
                       frameBoxShadowCssValue: 'unset',
+                      ...userExpressiveCodeConfig.styleOverrides?.frames,
                     },
                     textMarkers: {
                       backgroundOpacity: '40%',
                       markBackground: 'var(--sl-rapide-ec-marker-bg-color)',
                       markBorderColor: 'var(--sl-rapide-ec-marker-border-color)',
+                      ...userExpressiveCodeConfig.styleOverrides?.textMarkers,
                     },
                   },
-                  themes: ['vitesse-dark', 'vitesse-light'],
-                  ...(typeof config.expressiveCode === 'object' ? config.expressiveCode : {}),
                 },
         })
       },
